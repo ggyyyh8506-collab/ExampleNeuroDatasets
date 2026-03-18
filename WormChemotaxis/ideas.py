5aed1997a47336e8afe62239f9c3b75cb595a7c1
Padraig Gleeson <p.gleeson@gmail.com>
2026-03-18 11:52:59 +0000

    Improved plotting

--- WormChemotaxis/ideas.py
+++ WormChemotaxis/ideas.py
@@ -4,37 +4,53 @@ import h5py
 import os
 import pandas as pd
 import matplotlib.pyplot as plt
+import sys
 
 # Open file
-filepath = (
-    "Chemotaxis-Data-and-Analysis/Mock_worms/chemotaxis_mock_25825_1_20250725_153953/metadata_featuresN_oneworm.hdf5"
-)
-filepath = "Chemotaxis-Data-and-Analysis/Aversive_worms/chemotaxis_avsv_24_1_23_03_20240124_142324/metadata_featuresN_oneworm.hdf5"
+filepath = "Chemotaxis-Data-and-Analysis/Mock_worms/chemotaxis_mock_25825_1_20250725_153953/metadata_featuresN_oneworm.hdf5"
+if '-a' in sys.argv:
+    filepath = "Chemotaxis-Data-and-Analysis/Aversive_worms/chemotaxis_avsv_24_1_23_03_20240124_142324/metadata_featuresN_oneworm.hdf5"
+if '-s' in sys.argv:
+    filepath = 'Chemotaxis-Data-and-Analysis/sexually_conditioned_worms/chemotaxis_sexc_24_1_26_13_20240126_152252/metadata_featuresN_oneworm.hdf5'
+
 
 # Check that file exists
 if not os.path.exists(filepath):
-    print(f"\nFile not found: {filepath}!\nPlease clone the repository into this folder using:\n")
-    print("    git clone -b Worm-Locomotion https://github.com/Barrios-Lab/Chemotaxis-Data-and-Analysis.git\n")
+    print(
+        f"\nFile not found: {filepath}!\nPlease clone the repository into this folder using:\n"
+    )
+    print(
+        "    git clone -b Worm-Locomotion https://github.com/Barrios-Lab/Chemotaxis-Data-and-Analysis.git\n"
+    )
     quit()
 
 with h5py.File(filepath, "r") as f:
+    print("Loaded dataset from:", filepath)
     # List all datasets
-    print("Datasets:", list(f.keys()))
+    print("Keys for datasets:", list(f.keys()))
 
     # Load trajectories data as DataFrame
     traj_data = pd.DataFrame(f["trajectories_data"][:])
+    print(
+        f"Trajectory data loaded with shape: {traj_data.shape} and columns: {traj_data.columns.tolist()}"
+    )
 
     # Load timeseries features (if present)
     if "timeseries_data" in f:
         timeseries = pd.DataFrame(f["timeseries_data"][:])
 
+    base_coordinates = f["base_coordinates"][:]
+    print(f"Base coordinates: {base_coordinates}")
+
     # Load odor patch coordinates
     if "food_cnt_coord" in f:
         odor_patch = f["food_cnt_coord"][:]
-        print(f"Odor patch defined by {len(odor_patch)} boundary points")
+        print(
+            f"Odor patch defined by {len(odor_patch)} boundary points, ({odor_patch[0]}, {odor_patch[1]}, ..., {odor_patch[-1]})"
+        )
 
     # Load skeleton coordinates
-    #coords = f['coordinates'][:]  # Shape: (n_frames, n_segments, 2)
+    # coords = f['coordinates'][:]  # Shape: (n_frames, n_segments, 2)
 
 print(odor_patch)
 
@@ -43,8 +59,17 @@ plt.figure(figsize=(6, 6))
 
 plt.plot(odor_patch[:, 0], odor_patch[:, 1], "r-", label="Odor Patch Boundary")
 
+
 plt.scatter(
-    traj_data["coord_x"], traj_data["coord_y"], s=10, c="blue", label="Worm Trajectory"
+    traj_data["coord_x"], traj_data["coord_y"], s=1, c="blue", label="Worm Trajectory"
+)
+
+plt.scatter(
+    base_coordinates[:, 0],
+    base_coordinates[:, 1],
+    s=1,
+    c="green",
+    label="Worm base Trajectory",
 )
 
 plt.xlabel("X Coordinate")
